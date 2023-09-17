94e419bf4fafc0af04a757d4993da40c6b245a91
Rafa Peña <rafa@rafapenya.com>
2023-09-17 16:14:04 +0200

    A

--- nest-backend/dist/main.js
+++ nest-backend/dist/main.js
@@ -7,9 +7,21 @@ const helmet_1 = require("helmet");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
     app.enableCors({
-        origin: 'http://www.rafapenya.com',
+        origin: ['http://www.rafapenya.com', 'http://www.rafapenya.com/telocambio'],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-        allowedHeaders: 'Content-Type',
+        credentials: true,
+    });
+    app.enableCors({ origin: /.+/ });
+    app.use((req, res, next) => {
+        if (req.method === 'OPTIONS') {
+            res.header('Access-Control-Allow-Origin', 'http://www.rafapenya.com');
+            res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+            res.header('Access-Control-Allow-Headers', 'Content-Type');
+            res.header('Access-Control-Allow-Credentials', 'true');
+            res.status(200).end();
+            return;
+        }
+        next();
     });
     app.use(compression());
     app.use((0, helmet_1.default)());
